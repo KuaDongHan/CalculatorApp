6d6efba60b9f7658c009c9d06c25931bb1920a35
KuaDongHan <46645441+KuaDongHan@users.noreply.github.com>
2020-12-03 10:44:35 +0800

    Update test_my_calculator_functions_addition.py

--- test_my_calculator_functions_addition.py
+++ test_my_calculator_functions_addition.py
@@ -14,7 +14,7 @@ def test_add_negative_positive():
 
 def test_add_negative_negative():
     value = add(-1,-2)
-    assert value == -5
+    assert value == -3
 #hello
 
 def test_over_ten_variables():#add comment
